4de781ba4decb55a6f36efcbe3002ba490222cae
Raymond W. Ko <raymond.w.ko@gmail.com>
2019-02-02 20:55:11 -0500

    _make.bat: update to python 3.8

--- _make.bat
+++ _make.bat
@@ -2,8 +2,8 @@ PUSHD %~dp0
 REM C:\cygwin\bin\bash -c "/usr/bin/hg purge --all"
 CD src
 
-SET SDK_INCLUDE_DIR=C:\Program Files\Microsoft SDKs\Windows\v7.1\Include
-SET VC_DIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+REM SET SDK_INCLUDE_DIR=C:\Program Files\Microsoft SDKs\Windows\v7.1\Include
+SET VC_DIR="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build
 
 if "%VCINSTALLDIR%"=="" (CALL %VC_DIR%\vcvarsall.bat" amd64)
 
@@ -13,7 +13,7 @@ REM PYTHON=C:\opt\Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 ^
 SET VIM_CONFIG_OPTIONS=^
 FEATURES=HUGE GUI=yes DIRECTX=yes OLE=yes MBYTE=yes ^
 IME=yes DYNAMIC_IME=yes GIME=yes ^
-PYTHON3=C:\opt\Python36 DYNAMIC_PYTHON3=yes PYTHON3_VER=36 ^
+PYTHON3=C:\Python38 DYNAMIC_PYTHON3=yes PYTHON3_VER=38 ^
 SNIFF=yes CSCOPE=yes ICONV=yes GETTEXT=yes POSTSCRIPT=yes ^
 NETBEANS=yes XPM=no ^
 CPU=AMD64 OPTIMIZE=MAXSPEED CPUNR=pentium4 WINVER=0x0500 ^
